e654a3b9b9170ac71d0e5455c0db5d0bdc0b39c3
Joel Funkhouser <tech@enspiremedia.com>
2019-07-05 21:09:22 -0700

    Initial Test of Exported Components

---
+++ src/components/fancy.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export class Fancy extends React.Component {
+  render() {
+    return (
+      <div>This is so Fancy!</div>
+    );
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+export { Fancy } from './components/fancy';
+export { Spinner } from './components/spinner';
