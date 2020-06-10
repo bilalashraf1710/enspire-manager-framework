db8bfb5e450bb3638d934bf946b89f5fe8967205
Joel Funkhouser <tech@enspiremedia.com>
2020-06-10 15:35:14 -0700

    Updates to Sweet Modal Alert

--- src/components/modal_alert.js
+++ src/components/modal_alert.js
@@ -5,11 +5,16 @@ export function ModalAlert(options) {
 		text: options.text,
 		type: options.type,
 		showCancelButton: (options.show_cancel !== undefined) ? options.show_cancel : true,
-		confirmButtonColor: "#DD6B55",
+		confirmButtonColor: (options.confirm_color !== undefined) ? options.confirm_color : "#DD6B55",
 		confirmButtonText: (options.confirm_text !== undefined) ? options.confirm_text : "Yes, delete it!",
+		cancelButtonText: (options.cancel_text !== undefined) ? options.cancel_text : "Cancel",
 		closeOnConfirm: true,
-	}, () => {
-		if (typeof options.callback === 'function') options.callback();
+	}, (result) => {
+		if (result) {
+			if (typeof options.callback_success === 'function') options.callback_success();
+		} else {
+			if (typeof options.callback_cancel === 'function') options.callback_cancel();
+		}
 	});
 
 	return null;
