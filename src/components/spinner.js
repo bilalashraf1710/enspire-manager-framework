d3300773d933f04c27ede4454f563c7b8c8b7b4e
Joel Funkhouser <tech@enspiremedia.com>
2021-08-23 07:55:57 -0700

    Updates to Widgets & Spinner

--- src/components/spinner.js
+++ src/components/spinner.js
@@ -11,14 +11,16 @@ export function Spinner(props) {
 		zIndex: 2000,
 	} : {};
 
+	var color = (props.color) ? { backgroundColor: props.color } : {};
+
 	return (
 
 		<div className="sk-spinner sk-spinner-wave" style={{ ...style, ...props.style  }}>
-			<div className="sk-rect1"></div>
-			<div className="sk-rect2"></div>
-			<div className="sk-rect3"></div>
-			<div className="sk-rect4"></div>
-			<div className="sk-rect5"></div>
+			<div className="sk-rect1" style={ color }></div>
+			<div className="sk-rect2" style={ color }></div>
+			<div className="sk-rect3" style={ color }></div>
+			<div className="sk-rect4" style={ color }></div>
+			<div className="sk-rect5" style={ color }></div>
 		</div>
 	);
 }
--- src/components/widget.js
+++ src/components/widget.js
@@ -1,24 +1,29 @@
 import React from 'react';
+import { Spinner } from './spinner';
 
 export function Widget(props) {
 
-	var cursor = (props.onClick) ? 'pointer' : 'auto'; 
+	var cursor = (props.onClick) ? 'pointer' : 'auto';
 	var color_class;
 	switch (props.color_number) {
-		case 0 : color_class = 'bg-muted'; break;
-		case 1 : color_class = 'bg-primary'; break;
-		case 2 : color_class = 'bg-success'; break;
-		case 3 : color_class = 'bg-info'; break;
-		case 4 : color_class = 'bg-warning'; break;
-		case 5 : color_class = 'bg-danger'; break;
+		case 0: color_class = 'bg-muted'; break;
+		case 1: color_class = 'bg-primary'; break;
+		case 2: color_class = 'bg-success'; break;
+		case 3: color_class = 'bg-info'; break;
+		case 4: color_class = 'bg-warning'; break;
+		case 5: color_class = 'bg-danger'; break;
 	}
+
+	var spinnerColor = (props.spinnerColor) ? props.spinnerColor : '';
 
 	return (
 
 		<div className={ props.grid }>
 			{ props.type == 2
-				?	<div className={ 'widget style1 animated fadeInRight '+props.className+' '+color_class } style={{ cursor }} onClick={ props.onClick }>
-						<div className="row">
+				? <div className={ 'widget style1 animated fadeInRight ' + props.className + ' ' + color_class } style={ { cursor: cursor, minHeight: '100px' } } onClick={ props.onClick }>
+					{ props.fetching
+						? <Spinner center color={ spinnerColor } />
+						: <div className="row">
 							<div className="col-4">
 								<i className={ props.icon }></i>
 							</div>
@@ -27,14 +32,18 @@ export function Widget(props) {
 								<h2 className="font-bold">{ props.title }</h2>
 							</div>
 						</div>
-					</div>
-				:	<div className={ 'widget p-lg text-center animated fadeInRight '+props.className+' '+color_class} style={{ cursor }} onClick={ props.onClick }>
-						<div className="m-b-md">
+					}
+				</div>
+				: <div className={ 'widget p-lg text-center animated fadeInRight ' + props.className + ' ' + color_class } style={ { cursor: cursor, minHeight: '100px' } } onClick={ props.onClick }>
+					{ props.fetching
+						? <Spinner center color={ spinnerColor } />
+						: <div>
 							<i className={ props.icon }></i>
 							<h1 className="m-xs">{ props.title }</h1>
 							<h3 className="font-bold no-margins">{ props.text }</h3>
 						</div>
-					</div>
+					}
+				</div>
 			}
 		</div>
 	);
