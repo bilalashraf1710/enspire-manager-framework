1ed4b0856196ab8b4960dcbb8de8a65942928975
Joel Funkhouser <tech@enspiremedia.com>
2021-08-23 14:20:10 -0700

    Updates to iBox

--- src/components/ibox.js
+++ src/components/ibox.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Spinner } from './spinner';
+import { Select } from './form-elements/select';
 
 export function Ibox(props) {
 
 	var styleIbox = (props.mini) ? { marginBottom: '12px' } : {};
 	var styleTitle = (props.mini) ? { padding: '10px', minHeight: 'auto', maxHeight: '34px' } : {};
 	var styleContent = (props.mini) ? { padding: '10px' } : {};
+
+	var options = props.dropdown_array.map((option, index) => {
+		return (
+			<option key={ 'option' + index } value={ option }>{ option }</option>
+		);
+	});
 
 	return (
 
@@ -14,11 +21,11 @@ export function Ibox(props) {
 				<div className="ibox-title" style={ styleTitle }>
 					<h5>{ props.title }
 						{ props.icon_array &&
-							<div className="ibox-tools" style={{ marginTop: '-5px' }}>
-								{ 
+							<div className="ibox-tools" style={ { marginTop: '-5px' } }>
+								{
 									props.icon_callback_array?.slice().map((callback, index) => {
 										if (typeof callback === "function") return (
-											<a key={ 'icon'+index } onClick={ () => callback() } className="ml-2" style={{ fontSize: (props.mini) ? '14px' : '18px' }}>
+											<a key={ 'icon' + index } onClick={ () => callback() } className="ml-2" style={ { fontSize: (props.mini) ? '14px' : '18px' } }>
 												<i className={ props.icon_array.slice()[index] } style={ { color: '#c4c4c4' } }></i>
 											</a>
 										);
@@ -27,12 +34,24 @@ export function Ibox(props) {
 							</div>
 						}
 						{ props.button &&
-							<button type="button" className="btn btn-sm btn-primary ml-3 pull-right" style={{ marginTop: '-4px' }} onClick={ props.button_callback.bind(this) }>{ props.button }</button>
+							<button type="button" className="btn btn-sm btn-primary ml-3 pull-right" style={ { marginTop: '-4px' } } onClick={ props.button_callback.bind(this) }>{ props.button }</button>
+						}
+						{ props.dropdown_callback &&
+							<div className={ 'ibox-tools' } style={ { marginTop: '-7px' } }>
+								< Select
+									noLabel={ true }
+									name={ 'dropdown' }
+									onChange={ props.dropdown_callback }
+									value={ props.dropdown_value }
+								>
+									{ options }
+								</Select>
+							</div>
 						}
 					</h5>
 				</div>
 				<div className={ 'ibox-content' + ((props.no_fade) ? ' no-fade' : '') + ((props.show_spinner) ? ' sk-loading' : '') } style={ styleContent }>
-					
+
 					<Spinner />
 
 					<div className="px-2">
