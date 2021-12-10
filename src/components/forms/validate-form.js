386a33f2186c4e2c1f51f9db5fac372f7aa3e250
Joel Funkhouser <tech@enspiremedia.com>
2021-12-10 12:54:35 -0800

    Exclude input type 'skip' form validation

--- src/components/forms/validate-form.js
+++ src/components/forms/validate-form.js
@@ -14,7 +14,7 @@ export function ValidateForm(record, form_builder_layout) {
 				});
 			});
 		});
-
+        
 	} else if (form_builder_layout[0].block) {
 
 		form_builder_layout.forEach((block, block_index) => {
@@ -28,49 +28,53 @@ export function ValidateForm(record, form_builder_layout) {
 		});
 	}
 
+    return form_error;
+
 	function validate(field, record) {
 
-		var value = null;
+        var value = null;
 
-		/* Required -----------------------------------------*/
-		if (field.valid && field.valid.includes('required') && !field.disabled) {
-			value = (typeof record[field.field] == 'string') ? record[field.field].trim() : record[field.field];
-			if (typeof value == 'string') value = value.trim();
-			if (!value) form_error.push({ field: field.field, type: 'required' });
-		}
+		if (field.type != 'skip') {
 
-		/* numeric -----------------------------------------*/
-		if (field.valid && field.valid.includes('numeric') && !field.disabled) {
-			value = (typeof record[field.field] == 'string') ? record[field.field].trim() : record[field.field];
-			if (value && isNaN(value)) {
-				form_error.push({ field: field.field, type: 'numeric' });
-			}
-		}
+            /* Required -----------------------------------------*/
+            if (field.valid && field.valid.includes('required') && !field.disabled) {
+                value = (typeof record[field.field] == 'string') ? record[field.field].trim() : record[field.field];
+                if (typeof value == 'string') value = value.trim();
+                if (!value) form_error.push({ field: field.field, type: 'required' });
+            }
 
-		/* email -----------------------------------------*/
-		if (field.valid && field.valid.includes('email') && !field.disabled) {
-			value = (typeof record[field.field] == 'string') ? record[field.field].trim() : record[field.field];
-			if (value && !isEmail(value)) {
-				form_error.push({ field: field.field, type: 'email' });
-			}
-		}
+            /* numeric -----------------------------------------*/
+            if (field.valid && field.valid.includes('numeric') && !field.disabled) {
+                value = (typeof record[field.field] == 'string') ? record[field.field].trim() : record[field.field];
+                if (value && isNaN(value)) {
+                    form_error.push({ field: field.field, type: 'numeric' });
+                }
+            }
 
-		/* leading_zeros -----------------------------------------*/
-		if (field.valid && field.valid.includes('leading_zeros') && !field.disabled) {
-			value = (typeof record[field.field] == 'string') ? record[field.field].trim() : record[field.field];
-			if (field.type == 'text' || field.type == 'textarea') {
-				if (value.toString().startsWith('0')) {
-					form_error.push({ field: field.field, type: 'leading_zeros' });
-				}
-			} else {
-				console.error('EM Validation: leading_zeros validation applies only to text or textarea fields');
-			}
-		}
+            /* email -----------------------------------------*/
+            if (field.valid && field.valid.includes('email') && !field.disabled) {
+                value = (typeof record[field.field] == 'string') ? record[field.field].trim() : record[field.field];
+                if (value && !isEmail(value)) {
+                    form_error.push({ field: field.field, type: 'email' });
+                }
+            }
 
-		/* Additional Validations here ----------------------*/
-		/* ... */
-		// console.log(form_error);
+            /* leading_zeros -----------------------------------------*/
+            if (field.valid && field.valid.includes('leading_zeros') && !field.disabled) {
+                value = (typeof record[field.field] == 'string') ? record[field.field].trim() : record[field.field];
+                if (field.type == 'text' || field.type == 'textarea') {
+                    if (value.toString().startsWith('0')) {
+                        form_error.push({ field: field.field, type: 'leading_zeros' });
+                    }
+                } else {
+                    console.error('EM Validation: leading_zeros validation applies only to text or textarea fields');
+                }
+            }
+            
+            /* Additional Validations here ----------------------*/
+            /* ... */
+            // console.log(form_error);
+        }
 	}
 
-	return form_error;
 }
